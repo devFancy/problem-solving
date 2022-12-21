86eee31a828746939a92e54d673e148e48a31da5
fancy <mn1121m@knu.ac.kr>
2022-12-21 23:59:17 +0900

    [level 1] Title: 모의고사, Time: 2.72 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] answers) {
+        
+        int [] s1 = {1,2,3,4,5};
+        int [] s2 = {2,1,2,3,2,4,2,5};
+        int [] s3 = {3,3,1,1,2,2,4,4,5,5}; 
+        
+        int [] score = new int[3];
+        for(int i = 0; i < answers.length; i++) {
+            if(answers[i] == s1[i % s1.length]) {
+                score[0]++;
+            }
+            if(answers[i] == s2[i % s2.length]) {
+                score[1]++;
+            }
+            if(answers[i] == s3[i % s3.length]) {
+                score[2]++;
+            }
+        }
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        if(maxScore == score[0]) { list.add(1);}
+        if(maxScore == score[1]) { list.add(2);}
+        if(maxScore == score[2]) { list.add(3);}
+        return list.stream().mapToInt(i->i.intValue()).toArray();
+    }
+}
