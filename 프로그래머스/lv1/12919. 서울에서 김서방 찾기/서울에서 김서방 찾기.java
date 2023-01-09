fe993e7a42d479a54a70b36a2ac674500c66fbca
Junyong Moon <mn1121m@knu.ac.kr>
2023-01-10 00:33:47 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 11.50 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,9 +1,12 @@
-import java.util.*;
-
 class Solution {
     public String solution(String[] seoul) {
-        int x = Arrays.asList(seoul).indexOf("Kim");
-
-        return "김서방은 "+ x + "에 있다";
+        String answer = "";
+        for(int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+        return answer;
     }
 }
