0e5356139cce9eb3e2927d32af498ac0a20715c2
Junyong Moon <mn1121m@knu.ac.kr>
2023-01-14 12:28:20 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,11 +1,8 @@
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        double result = Math.sqrt(n);
-        if(result % 1 == 0) {
-            answer = ((long)result+1) * ((long)result+1);
-        } else 
-            answer = -1;
-        return answer;
+        if(Math.pow((int)Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n)+1, 2);
+        }
+        return -1;
     }
 }
