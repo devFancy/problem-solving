94b383d4fdec9f9d3fe63d122fe37fec5e258cb8
Junyong Moon <mn1121m@knu.ac.kr>
2023-01-20 22:21:16 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.28 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,14 +1,9 @@
 class Solution {
     public int solution(String s) {
-        
-        String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};  
-        
-        for(int i=0; i<10; i++){
-            s = s.replace(num[i], Integer.toString(i));
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-        
-        int answer = Integer.parseInt(s);
-        
-        return answer;
+        return Integer.parseInt(s);
     }
 }
