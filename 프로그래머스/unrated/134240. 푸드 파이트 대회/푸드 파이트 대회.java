f92ef041446aecc9c66f81b5f7a36783f2adcf86
Junyong Moon <mn1121m@knu.ac.kr>
2023-01-20 22:33:26 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 2.17 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,22 +1,11 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        String left = "";
-        String right = "";
-        
+        StringBuilder builder = new StringBuilder();
         for(int i = 1; i < food.length; i++) {
-            if(food[i] != 0) {
-                for(int j = 0; j < (food[i] / 2); j++) {
-                    left += Integer.toString(i);
-                    right += Integer.toString(i);
-                }
-            }
+            int share = food[i] / 2;
+            builder.append(String.valueOf(i).repeat(share));
         }
-        answer = left + "0";
-        
-        for(int i = right.length()-1; i >= 0; i--) {
-            answer += right.substring(i, i+1);
-        }
-        return answer;
+        String answer = builder + "0";
+        return answer + builder.reverse();
     }
 }
