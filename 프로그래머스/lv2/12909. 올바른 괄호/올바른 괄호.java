425e0d026c8808ad833bf3bdf01123d638401e67
Junyong Moon <jymoon1121@gmail.com>
2023-02-04 02:52:19 +0900

    [level 2] Title: 올바른 괄호, Time: 7.90 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,37 +1,22 @@
-import java.util.*;
-
 class Solution {
     boolean solution(String s) {
-        // [1] 정답을 저장할 변수로 answer와 stack을 선언
-        boolean answer = true;
-        Stack <Character> stack = new Stack<>();
+        boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
+            }
+        }
+        if(count == 0){
+            answer = true;
+        }
 
-        // 반복문을 이용하여 문자열을 앞에서부터 비교
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            // [2] 문자를 비교할 때 `(` 일 경우 스택에 push 
-            if(c == '(') {
-                stack.push(c);
-            }
-            
-            // [3] `)`일 경우
-            if( c == ')') {
-                // stack의 크기가 0이면 false 반환
-                if(stack.size() == 0) {
-                    answer = false;
-                }
-                // stack의 크기가 0이 아니라면  `(`가 있다는 의미이므로 스택에 pop
-                else {
-                    stack.pop();
-                }
-            }
-        }
-        // [4] 문자열을 끝까지 확인한 이후에 스택의 크기가 0이면 true 반환
-        if(stack.size() != 0) {
-            answer = false;
-        }
-        
         return answer;
     }
 }
