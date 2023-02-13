17c5d5bc57503236ac2295814b3cb9ee446981f3
Junyong Moon <jymoon1121@gmail.com>
2023-02-13 15:52:50 +0900

    [level 2] Title: 타겟 넘버, Time: 0.31 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,26 @@
-/*
-* numbers : 알고리즘을 실행할 대상 배열
-* depth : 노드의 깊이
-* target : 타겟 넘버
-* sum : 이전 노드 까지의 결과 값
-*/
-
 class Solution {
-    int answer = 0;
+    int [] numbers;
+    int target;
+    int answer;
 
+    void dfs(int index, int sum) {
+        //1. 탈출 조건
+        if(index == numbers.length) {
+            if(sum == target) answer++;
+            return;
+        }
+        //2. 수행 동작
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, target, 0);
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+
+        dfs(0, 0);
 
         return answer;
-    }
-
-    // 깊이 우선 탐색
-    public void dfs(int[] numbers, int depth, int target, int sum){
-        // 마지막 노드 까지 탐색한 경우
-        if(depth == numbers.length){
-            if(target == sum) answer++;
-        } else {
-            // 해당 노드의 값을 더하고 다음 깊이 탐색
-            dfs(numbers, depth + 1, target, sum + numbers[depth]);
-            // 해당 노드의 값을 빼고 다음 깊이 탐색
-            dfs(numbers, depth + 1, target, sum - numbers[depth]);
-        }
     }
 }
