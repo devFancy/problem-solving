b1494fc604b2040306ca9f686955aa84511e4c33
Junyong Moon <jymoon1121@gmail.com>
2023-02-20 14:11:19 +0900

    [level 2] Title: [1차] 캐시, Time: 2.66 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 2.76 ms
+메모리: 81.4 MB, 시간: 2.66 ms
 
 ### 구분
 
--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,28 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        ArrayList<String> cache = new ArrayList<>();
         
-        if(cacheSize == 0) // 캐시크기가 0
+        List<String> cacheList = new ArrayList<>();
+        
+        if(cacheSize == 0) {
             return cities.length * 5;
+        }
         
-        for(int i=0; i<cities.length; i++) {
-            cities[i] = cities[i].toUpperCase(); // 대소문자 구분X
-            if(cache.contains(cities[i])) { // cache hit
-                cache.remove(cities[i]);
-                cache.add(cities[i]);
+        for(int i = 0; i < cities.length; i++) {
+            cities[i] = cities[i].toLowerCase(); // 소문자로 변환
+            //cache hit
+            if(cacheList.contains(cities[i])) {
+                cacheList.remove(cities[i]);
+                cacheList.add(cities[i]);
                 answer += 1;
             }
-            else { // cache miss
-                if(cache.size()==cacheSize) {
-                    cache.remove(0);
-                    cache.add(cities[i]);
-                }
-                else
-                    cache.add(cities[i]);
+            //cache miss
+            else {
                 answer += 5;
+                if(cacheList.size() == cacheSize) {
+                    cacheList.remove(0);
+                    cacheList.add(cities[i]);
+                }
+                else {
+                    cacheList.add(cities[i]);
+                }
             }
         }
+        
         return answer;
     }
 }
