330aa26a745979ed1c87e98179c90ef433789df0
Junyong Moon <jymoon1121@gmail.com>
2023-02-24 13:56:22 +0900

    [level 1] Title: 소수 찾기, Time: 17.91 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,25 +1,19 @@
 class Solution {
     public int solution(int n) {
-        
-        boolean flag; // 소수이면 true, 아니면 false
         int answer = 0;
-        
-        //에라토스테네스의 법칙
-        for(int i = 2; i <= n; i++) {
-            flag = true;
-            for(int j = 2; j <= Math.sqrt(i); j++) {
-                if(i%j == 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            // flag = true, 소수값이므로 +1 추가
-            if(flag == true) {
-                answer++;
+        int[] arr = new int[n + 1];
+
+        for (int i = 2; i <= (int) Math.sqrt(n); ++i) {
+            if (arr[i] == 1) continue;
+            for (int j = i + i; j <= n; j += i) {
+                arr[j] = 1;
             }
         }
         
+        for (int i = 2; i < arr.length; ++i) {
+            if (arr[i] != 1) ++answer;
+        }
+
         return answer;
-        
     }
 }
