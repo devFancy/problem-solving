70d78380fd94dbf176b3a91f32baae7be5974cea
Junyong Moon <jymoon1121@gmail.com>
2023-03-08 16:49:01 +0900

    [unrated] Title: 귤 고르기, Time: 46.69 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Collections;
+
 class Solution {
+    
     public int solution(int k, int[] tangerine) {
         int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        
+        // 크기별로 몇 개 있는지 map에 저장함
+        for (int tan : tangerine) {
+            map.put(tan, map.getOrDefault(tan, 0)+1);
+        }
+
+        // 개수(value)가 많은 순으로 정렬
+        List<Integer> list = new ArrayList<>(map.values());
+        Collections.sort(list, Collections.reverseOrder());
+
+        // 개수가 많은 순부터 사용
+        for (Integer a : list) {
+            if(k < 1) 
+                return answer;
+            k -= a;
+            answer++;
+        }
+
         return answer;
     }
 }
