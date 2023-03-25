db74c7c2a392d19ce06292c9bfc65e31b0cdb0e1
Junyong Moon <jymoon1121@gmail.com>
2023-03-25 16:58:57 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.43 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,19 +1,19 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder convert = new StringBuilder();
         StringBuilder answer = new StringBuilder();
 
-        int num = 0;
 
-        // m명이 t번 말하는 경우까지 구함
-        while(sb.length() < m * t) {
-            sb.append(Integer.toString(num++, n));
+        // convert의 길이가 미리 구할 숫자 갯수 * 게임 참가 인원만큼 될 때 까지 n진수로 변환
+        for(int i = 0; convert.length() <= t * m; i++){
+            convert.append(Integer.toString(i, n));
         }
 
+        // 튜브의 순서에 해당하는 글자들만 추출
         // 문자열은 0부터 이므로 p-1부터
         // 튜브가 말할 것만 알면되기 때문에 +m 씩 증가
         for(int i = p - 1; i < m * t; i = i + m) {
-            answer.append(sb.charAt(i));
+            answer.append(convert.charAt(i));
         }
         return answer.toString().toUpperCase();
     }
