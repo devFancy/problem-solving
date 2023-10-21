fe4cfc6e7e64bcd01f616986915ad3745b03a8e6
Junyong Moon <jymoon1121@gmail.com>
2023-10-21 23:47:19 +0900

    [level 2] Title: 택배상자, Time: 36.97 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> sub_container = new Stack<>(); // 보조 컨테이너 벨트
+        int container_box = 1; // 현재 메인 컨테이너에 있는 박스
+        for(int o : order) {
+            while(container_box <= order.length) {
+                if(container_box == o) break;
+                else if (!sub_container.isEmpty() && sub_container.peek() == o) break;
+                else {
+                    sub_container.push(container_box);
+                    container_box++;
+                }
+            } 
+        
+            if(container_box == o) {
+                container_box++;
+                answer++;
+            } else if(!sub_container.isEmpty() && sub_container.peek() == o) {
+                sub_container.pop(); 
+                answer++;
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
