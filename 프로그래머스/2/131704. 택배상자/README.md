672db3ac7a4ff47138b1398edad1db64dbcea8b8
Junyong Moon <jymoon1121@gmail.com>
2023-10-23 17:36:08 +0900

    [level 2] Title: 택배상자, Time: 33.14 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/README.md
+++ 프로그래머스/2/131704. 택배상자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배상자 - 131704 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131704#) 
 
 ### 성능 요약
 
-메모리: 135 MB, 시간: 33.91 ms
+메모리: 123 MB, 시간: 33.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 1일 16:4:13
+2023년 10월 1일 17:36:6
 
 ### 문제 설명
 
--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,31 +1,46 @@
 import java.util.*;
 
+/**
+* main_con : 메인 컨테이너
+* sub_con : 서브 컨테이너
+* 주문 갯수만큼 for 문 돌리면서
+* 1) 메인 컨테이너 오는 순서대로 while문을 돌리면서
+* 1-1) 메인 컨테이너의 현재 위치에 있는 값과 일치하면 break;
+* 1-2) 서브 컨테이너가 비어있지 않고, 서브 컨테이너의 현재 위치에 있는 값과 일치하면 break;
+* 1-3) 메인, 서브에 있는 값과 일치하지 않으면 1 증가시켜 1-1로 되돌아간다.
+* 2) 서브 컨테이너(Stack)에 다 저장되었으면 탐색 시작한다.
+* 2-1) 메인 컨테이너의 현재 위치에 있는 값과 일치하면 정답 갯수 +1 증가
+* 2-2) 서브 컨테이너가 비어있지 않고, 서브 컨테이너의 현재 위치에 있는 값과 일치하면 서브 컨테이너 pop()하고 정답 갯수 + 1 증가
+* 2-3) 메인, 서브에 있는 값과 일치하지 않으면 종료
+*/
+
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        Stack<Integer> sub_container = new Stack<>(); // 보조 컨테이너 벨트
-        int container_box = 1; // 현재 메인 컨테이너에 있는 박스
+        int main_con = 1;
+        Stack<Integer> sub_con = new Stack<>();
         for(int o : order) {
-            while(container_box <= order.length) {
-                if(container_box == o) break;
-                else if (!sub_container.isEmpty() && sub_container.peek() == o) break;
-                else {
-                    sub_container.push(container_box);
-                    container_box++;
+            while(main_con <= order.length) { // 1
+                if(main_con == o) break; // 1-1
+                else if(!sub_con.isEmpty() && sub_con.peek() == o) // 1-2
+                    break;
+                else { // 1-3
+                    sub_con.push(main_con);
+                    main_con++;
                 }
-            } 
-
-            if(container_box == o) {
-                container_box++;
+            }
+            if(main_con == o) { // 2-1
                 answer++;
-            } else if(!sub_container.isEmpty() && sub_container.peek() == o) {
-                sub_container.pop(); 
+                main_con++;
+            } else if(!sub_con.isEmpty() && sub_con.peek() == o) // 2-2
+            {
+                sub_con.pop();
                 answer++;
-            } else {
+            } else // 2-3
+            {
                 break;
             }
         }
-
         return answer;
     }
 }
