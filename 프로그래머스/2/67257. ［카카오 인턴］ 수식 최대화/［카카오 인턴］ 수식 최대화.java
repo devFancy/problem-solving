a3b8c05e4fcc3bbff45dd5c395a86126601bcbae
Junyong Moon <jymoon1121@gmail.com>
2023-11-09 13:02:28 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 11.40 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,92 +1,96 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
-    static List<List<String>> list = new ArrayList<>();
-    public long solution(String expression) {
-        long answer = 0;
-        boolean[] visited = new boolean[3];
-        String[] operationTypes = {"-","+","*"};
-        // 연산자 우선순위 모든 경우의 수 계산
-        permutation(new ArrayList<>(), operationTypes, visited);
-
-        // 연산자를 " "로 치환 후 split 값들을 Long 값으로 변환 후 toList
-        List<Long> numbers = Arrays.stream(expression.replaceAll("-|\\*|\\+"," ").split(" "))
-                .map(Long::parseLong).collect(Collectors.toList());
-        // 숫자를 공백으로 치환 후 toList
-        List<String> collect = Arrays.stream(expression.replaceAll("[0-9]", "").split(""))
-                .collect(Collectors.toList());
-
-        // 우선순위 모든 경우의 수에 따른 max 연산 값
-        for (List<String> strings : list) {
-            answer = Math.max(answer, solve(strings, numbers, collect));
+    static long answer = 0;
+    static String op[] = {"+", "-", "*"};
+    static boolean[] visited = new boolean[3];
+    static ArrayList<Long> numList = new ArrayList<>(); // 피연산자, 숫자
+    static ArrayList<String> opList = new ArrayList<>(); // 연산자
+    static String[] perm = new String[3];
+    
+    public long solution(String expresstion) {
+        String num = "";
+        
+        //num op 구분
+        for(int i = 0; i < expresstion.length(); i++) {
+            char c = expresstion.charAt(i);
+            if(c == '*' || c == '+' || c == '-') { // 연산자인 경우
+                opList.add(c+"");
+                numList.add(Long.parseLong(num));
+                num = ""; // num을 초기화합니다. 새로운 피연산자를 시작하기 위해서 num을 비워줍니다.
+            } else { // 피연산자인 경우
+                num += c;
+            }
         }
+        // 마지막 숫자
+        numList.add(Long.parseLong(num));
+        
+        // 순열 만들기
+        makePermutation(0);
+        
         return answer;
     }
+    
+    static void makePermutation(int depth) {
+        if(depth == op.length) {
+            // 3개를 선택 -> 연산
+            sol();
+            return;
+        }
+        for(int i = 0; i < op.length; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            perm[depth] = op[i];
+            makePermutation(depth+1);
+            visited[i] = false;
+        }
+    }
+    
+    static void sol() {
+        // list 복사
+        ArrayList<String> oper = new ArrayList<String>();
+        oper.addAll(opList);
+        
+        ArrayList<Long> num = new ArrayList<Long>();
+        num.addAll(numList);
+        
+        // 연산자 우선순위에 따라 계산
+        for(int i = 0; i < perm.length; i++) {
+            String op = perm[i];
+            for(int j = 0; j < oper.size(); j++) {
+                if(oper.get(j).equals(op)) {
+                    long n1 = num.get(j);
+                    long n2 = num.get(j+1);
+                    long res = cal(n1, n2, op);
 
-    static long solve(List<String> strings, List<Long> numbers, List<String> operations) {
-        // 리스트에 remove 메서드를 사용하기 위해서 복사
-        List<Long> numbersClone = new ArrayList<>(numbers);
-        List<String> operationsClone = new ArrayList<>(operations);
-
-        for (int i = 0; i < strings.size(); i++) {
-            String operation = strings.get(i); // 현재 순위의 연산자
-
-            for(int j = 0; j < operationsClone.size(); j++) {
-                // 현재 순위의 연산자와 같은 경우
-                if(operation.equals(operationsClone.get(j))) {
-                    // 연산자 앞의 수와 뒤의 수를 구한뒤 연산
-                    long front = numbersClone.get(j);
-                    long back = numbersClone.get(j + 1);
-                    long temp = calc(front, back, operation);
-
-                    // 연산에 사용된 두수를 제거
-                    numbersClone.remove(j+1);
-                    numbersClone.remove(j);
-
-                    // 연산에 사용된 연산자 제거
-                    operationsClone.remove(j);
-
-                    // 연산된 값을 추가
-                    numbersClone.add(j, temp);
-
-                    // 연산이 진행되는 경우 2개 -> 1개 가되므로 리스트의 사이즈에 변경이 일어남
-                    // 이를 맞추기 위해서 j를 --하여 다움 for문에서 범위를 벗어나지 않게 함
+                    // list 갱신
+                    num.remove(j+1);
+                    num.remove(j);
+                    oper.remove(j);
+                
+                    num.add(j, res);
+                
                     j--;
                 }
             }
         }
-        // 절대값으로 변환해서 리턴
-        return Math.abs(numbersClone.get(0));
+        
+        answer = Math.max(answer, Math.abs(num.get(0)));
     }
-
-    static void permutation(ArrayList<String> arrayList, String[] orders, boolean[] visited) {
-        if(arrayList.size() == 3) {
-            list.add(arrayList);
-            return;
+    
+    static long cal(long n1, long n2, String op) {
+        long result = 0;
+        switch(op) {
+            case "*":
+                result = n1 * n2;
+                break;
+            case "+":
+                result = n1 + n2;
+                break;
+            case "-":
+                result = n1 - n2;
+                break;
         }
-
-        for (int i = 0; i < orders.length; i++) {
-            if (!visited[i]) {
-                ArrayList<String> temp = new ArrayList<>(arrayList);
-                temp.add(orders[i]);
-                visited[i] = true;
-                permutation(temp, orders, visited);
-                visited[i] = false;
-            }
-        }
-    }
-
-    static long calc(long one, long two, String calc) {
-        switch (calc) {
-            case "-" :
-                return one - two;
-            case "+" :
-                return one + two;
-            default :
-                return one * two;
-        }
+        return result;
     }
 }
