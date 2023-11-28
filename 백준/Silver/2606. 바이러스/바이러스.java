f38ebc7ad58daac5ceb581fe2b19a427f51c9780
Junyong Moon <jymoon1121@gmail.com>
2023-11-28 14:31:48 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,55 +1,47 @@
+import java.io.*;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static boolean [] checked;
+    static int [][] arr;
+    static int node, line;
 
-	static boolean[] check;
-	static int[][] arr;
-	static int count = 0;
-	
-	static int node, line;
-	
-	static Queue<Integer> q = new LinkedList<>();
+    static int count = 0;
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
-		node = Integer.parseInt(br.readLine());
-		line = Integer.parseInt(br.readLine());
-	
-		arr = new int[node+1][node+1];
-		check = new boolean[node+1];
-		
-		for(int i = 0 ; i < line ; i ++) {
-			StringTokenizer str = new StringTokenizer(br.readLine());
-			
-			int a = Integer.parseInt(str.nextToken());
-			int b = Integer.parseInt(str.nextToken());
-			
-			arr[a][b] = arr[b][a] =  1;	
-		}
-		
-			dfs(1);
-			
-			System.out.println(count-1);
-		
-		}
-	public static void dfs(int start) {
-		
-		check[start] = true;
-		count++;
-		
-		for(int i = 0 ; i <= node ; i++) {
-			if(arr[start][i] == 1 && !check[i])
-				dfs(i);
-		}
-		
-	}
-	
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        node = Integer.parseInt(br.readLine());
+        line = Integer.parseInt(br.readLine());
+
+        arr = new int[node+1][node+1];
+        checked = new boolean[node+1];
+
+        for(int i = 0; i < line; i++) {
+            StringTokenizer str = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(str.nextToken());
+            int b = Integer.parseInt(str.nextToken());
+
+            arr[a][b] = arr[b][a] = 1;
+        }
+        dfs(1);
+        
+        System.out.println(count-1);
+    }
+    public static void dfs(int start) {
+        checked[start] = true;
+        count++;
+
+        for(int i = 0; i <= node; i++) {
+            if(arr[start][i] == 1 && !checked[i]) {
+                dfs(i);
+            }
+        }
+    }
 }
