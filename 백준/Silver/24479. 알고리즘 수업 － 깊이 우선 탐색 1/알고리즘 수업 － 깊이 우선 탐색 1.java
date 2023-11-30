45f66f8bb868b848483c4d77b92fbb86c6a0e391
Junyong Moon <jymoon1121@gmail.com>
2023-11-30 15:36:09 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1020 ms, Memory: 91312 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,11 +1,12 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
     static final int MAX = 100000 + 10;
     static ArrayList<Integer>[] graph;
     static boolean[] visited;
-    static int N, M, R; // 정점(N)과 간선(M)
+    static int N, M, R;
     static int[] answer;
     static int order;
     public static void dfs(int idx) {
@@ -16,12 +17,11 @@ public class Main {
         for(int i = 0; i < graph[idx].size(); i++) {
             int next = graph[idx].get(i);
             if(visited[next] == false) {
-                dfs(graph[idx].get(i));
+                dfs(next);
             }
         }
     }
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         // 0. 입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -31,29 +31,29 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
 
-        // 1. graph에 연결 정보 채우기
+        // 1. graph에 연결정보 채우기
         graph = new ArrayList[MAX];
         for(int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
         }
         visited = new boolean[MAX];
-        answer = new int [MAX];
+        answer = new int[MAX];
         order = 1;
 
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            graph[x].add(y);
-            graph[y].add(x);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
         }
-
+        
         // 2. 오름차순 정렬
         for(int i = 1; i <= N; i++) {
             Collections.sort(graph[i]);
         }
 
-        // 3. dfs(재귀함수 호출)
+        // 3. dfs 호출
         dfs(R);
 
         // 4. 출력
@@ -61,7 +61,6 @@ public class Main {
             bw.write(String.valueOf(answer[i]));
             bw.newLine();
         }
-
         bw.close();
         br.close();
     }
