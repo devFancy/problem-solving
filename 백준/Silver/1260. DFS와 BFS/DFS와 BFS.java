c965af85a29e29f8cdfb0320252d7763c8402782
Junyong Moon <jymoon1121@gmail.com>
2023-11-30 17:03:11 +0900

    [Silver II] Title: DFS와 BFS, Time: 360 ms, Memory: 21624 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,76 +1,80 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static int [][] check; //간선 연결상태
-    static boolean [] checked; //확인 여부
-    static int n; //정점 개수
-    static int m; //간선 개수
-    static int start; // 시작정점
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Main {
+    static final int MAX = 1000 + 10;
+    static boolean[][] graph;
+    static boolean[] visited;
+    static ArrayList<Integer> queue; // bfs를 위해 만든 것
+    static int N, M, V;
+
+    public static void dfs(int idx) {
+        visited[idx] = true;
+        System.out.print(idx + " "); // 몇 번째 노드를 방문했다.
         
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(st.nextToken());
-        
-        check = new int[1001][1001]; // 좌표를 그대로 받아들이기 위해 +1 해서 선언
-        checked = new boolean[1001]; // 초기값 : false
-        
-        //간선 연결상태 저장
-        for(int i = 0; i < m; i++) {
-            StringTokenizer str = new StringTokenizer(br.readLine());
-            
-            int x = Integer.parseInt(str.nextToken());
-            int y = Integer.parseInt(str.nextToken());
-            
-            check[x][y] = check[y][x] = 1;
-        }
-        
-        //dfs 호출
-        dfs(start);
-        
-        checked = new boolean[1001]; // 확인상태 초기화
-        System.out.println(); // 줄바꿈
-        
-        //bfs 호출
-        bfs(start);
-    }
-    
-    //시작점을 변수로 받아 확인, 출력 후 다음 연결점을 찾아 시작점을 변경하여 재호출
-    public static void dfs(int start) {
-        checked[start] = true;
-        System.out.print(start + " ");
-        
-        for(int j = 1; j <= n; j++) {
-            if(check[start][j] == 1 && checked[j] == false) {
-                dfs(j);
+        for(int i = 1; i <= N; i++) {
+            if(visited[i] == false && graph[idx][i] == true) {
+                dfs(i);
             }
         }
+        
     }
-    
-    public static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        
-        queue.offer(start); // 시작점도 Queue에 넣어야 함
-        checked[start] = true;
-        
-        System.out.print(start + " ");
-        
-        //Queue가 비어있을 때까지 반복. 방문 정점은 확인, 출력 후 Queue에 넣어 순서대로 확인
-        while(!queue.isEmpty()) {
-            int temp = queue.poll();
-            
-            for(int j = 1; j <= n; j++) {
-                if(check[temp][j] == 1 && checked[j] == false) {
-                    queue.offer(j);
-                    checked[j] = true;
-                    System.out.print(j + " ");
+    /*
+     * queue를 만들고, visited를 새로 초기화해준다.
+     * 가장 첫 번째 시작 정점이라는 값(1)을 추가해주고, 그 값(1)을 방문했다라고 표기해준다. (처음 V 값이 1이므로)
+     * !queue.isEmpty() 때까지, 가장 앞에 있는 값을 꺼내오고, 출력해 준 다음에 그 값을 기준으로 방문할 수 있는 값들을 방문한다.
+     * 
+     */
+    public static void bfs() {
+        queue = new ArrayList<>();
+        visited = new boolean[MAX];
+
+        queue.add(V);
+        visited[V] = true;
+
+        while (!queue.isEmpty()) {
+            int idx = queue.remove(0); // 가장 앞에 있는 녀석을 꺼내서 걔를 인덱스에 담겠다.
+            System.out.print(idx + " ");
+
+            for(int i = 1; i <= N; i++) {
+                if(visited[i] == false && graph[idx][i] == true) {
+                    queue.add(i);
+                    visited[i] = true;
                 }
             }
+            
         }
     }
-    
+    public static void main(String[] args) throws IOException {
+        // 0. 입력 및 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+
+        graph = new boolean[MAX][MAX];
+        visited = new boolean[MAX];
+
+        // 1. graph에 연결 정보 채우기
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x][y] = true;
+            graph[y][x] = true;
+        }
+
+        // 2. dfs(재귀 함수) 호출 + 출력하기
+        dfs(V);
+        System.out.println();
+
+        // 3. bfs(너비 우선 탐색) 호출 + 출력하기
+        bfs();
+
+        bw.close();
+        br.close();
+    }
 }
