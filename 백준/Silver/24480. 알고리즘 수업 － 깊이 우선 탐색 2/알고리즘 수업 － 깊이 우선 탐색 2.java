c36dafb8178f3cfe9d7b03f395f4ba66f957150d
Junyong Moon <jymoon1121@gmail.com>
2023-11-30 16:05:39 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1104 ms, Memory: 93168 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final int MAX = 100000 + 10;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int N, M, R;
+    static int[] answer;
+    static int order;
+    public static void dfs(int idx) {
+        visited[idx] = true;
+        answer[idx] = order;
+        order++;
+
+        for(int i = 0; i < graph[idx].size(); i++) {
+            int next = graph[idx].get(i);
+            if(visited[next] == false) {
+                dfs(next);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        // 0. 입력 및 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        // 1. graph에 연결 정보 채우기
+        graph = new ArrayList[MAX];
+        for(int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        visited = new boolean[MAX];
+        answer = new int[MAX];
+        order = 1;
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        
+        // 2. 내림차순 정렬
+        for(int i = 1; i <= N; i++) {
+            Collections.sort(graph[i], Collections.reverseOrder());
+            // 내림차순으로 해달라는 의미에서 reverseOrder() 함수를 써주면 된다.
+        }
+
+        // 3. dfs 호출
+        dfs(R);
+
+        // 4. 출력
+        for(int i = 1; i <= N; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.newLine();
+        }
+        bw.close();
+        br.close();
+    }
+}
