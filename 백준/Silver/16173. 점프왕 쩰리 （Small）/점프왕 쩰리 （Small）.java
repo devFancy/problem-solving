86527bcd56044af6ad10593a459675635cde407c
Junyong Moon <jymoon1121@gmail.com>
2023-12-12 15:46:07 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -1,30 +1,27 @@
-// 점프왕 젤리
 import java.util.*;
 import java.io.*;
 
-class Main {
-    static final int MAX = 3 + 100 + 10;
-    static int map[][];
-    static boolean visited[][];
+public class Main {
+    static final int MAX = 3 + 110;
+    static int[][] map;
+    static int[] dirX = {0, 1};
+    static int[] dirY = {1, 0};
+    static boolean[][] visited;
     static int N;
-    static int dirY[] = {1, 0};
-    static int dirX[] = {0, 1};
-
     public static void dfs(int y, int x) {
         visited[y][x] = true;
-
-        if(y == N && x == N) 
-            return;
+        if(y == N && x == N) return;
 
         for(int i = 0; i < 2; i++) {
             int newY = y + dirY[i] * map[y][x];
             int newX = x + dirX[i] * map[y][x];
+
             if(visited[newY][newX] == false) {
                 dfs(newY, newX);
             }
         }
-    }
 
+    }
     public static void main(String[] args) throws IOException {
         // 0. 입력 및 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,7 +31,7 @@ class Main {
         map = new int[MAX][MAX];
         visited = new boolean[MAX][MAX];
 
-        // 1. map에 정보 반영
+        // 1. map에 정보 반영하기 
         for(int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 1; j <= N; j++) {
@@ -42,14 +39,13 @@ class Main {
             }
         }
 
-
-        // 2. dfs 수행
+        // 2. dfs 호출
         dfs(1,1);
 
-        // 3. 출력
-        if(visited[N][N]) bw.write("HaruHaru");
-        else bw.write("Hing");
-
+        // 3. 출력하기
+        if(visited[N][N] == false) bw.write("Hing");
+        else bw.write("HaruHaru");
+        
         bw.close();
         br.close();
     }
