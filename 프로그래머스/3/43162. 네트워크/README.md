3613c0400df28fe0bdbba580e65c508b67c1ea56
Junyong Moon <jymoon1121@gmail.com>
2023-12-14 12:55:57 +0900

    [level 3] Title: 네트워크, Time: 0.10 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#) 
 
 ### 성능 요약
 
-메모리: 66.3 MB, 시간: 0.20 ms
+메모리: 71.4 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 16:29:55
+2023년 12월 4일 12:55:55
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,37 @@
+// 프로그래머스 - 네트워크
 class Solution {
+    static boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[computers.length];
-        
+        visited = new boolean[computers.length];
+
         // 노드 방문 초기화
-        for (boolean visit : visited) {
-        visit = false;
+        for(boolean visit : visited) {
+            visit = false;
         }
-        
+
         for(int i = 0; i < computers.length; i++) {
-            if(visited[i] == false) { // 해당 노드를 방문하지 않았을 경우
-            
-                dfs(i, visited, computers);
-                answer++; // 새로운 네트워크 찾았으므로 +1 증가
+            if(visited[i] == false) {
+                answer ++;
+                dfs(i, computers);
             }
         }
-        
+
         return answer;
     }
-    // node: 현재 노드, visited: 방문여부, computers: 컴퓨터간의 연결 정보를 나타내는 2차원 배열
-    public void dfs(int node, boolean[] visited, int[][] computers) {
+
+    /*
+     * node: 현재 노드
+     * visited: 방문여부
+     * computer: 컴퓨터간의 연결 정보를 나타내는 2차원 배열
+     */
+    public void dfs(int node, int[][] computers) {
         visited[node] = true;
-        
+
         for(int i = 0; i < computers.length; i++) {
-            // 아직 방문하지 않는 노드 && 현재 노드와 연결된 경우
-            if(visited[i] == false & computers[node][i] == 1) {
-                dfs(i, visited, computers);
+            if(visited[i] == false && computers[node][i] == 1) {
+                dfs(i, computers);
             }
         }
     }
