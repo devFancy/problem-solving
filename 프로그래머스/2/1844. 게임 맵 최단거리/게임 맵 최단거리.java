e5db5d8f57702e58766f3f06d5354f2c0d5bc227
Junyong Moon <jymoon1121@gmail.com>
2023-12-14 14:02:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.97 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,53 @@
 import java.util.*;
 
 class Solution {
-  public class position {
-    int x;
-    int y;
-
-    public position(int x, int y) {
-      this.x = x;
-      this.y = y;
-    }
-  }
-
-  int[] nx = {-1, 1, 0, 0};
-  int[] ny = {0, 0, -1, 1};
-  public int solution(int[][] maps) {
-    int h = maps.length;
-    int w = maps[0].length;
-
-    Queue<position> q = new LinkedList<>();
-    q.add(new position(0,0));
-
-    boolean[][] visited = new boolean[h][w];
-    visited[0][0] = true;
-
-    position p;
-    while (!q.isEmpty()) {
-      p = q.poll();
-      int x = p.x;
-      int y = p.y;
-
-      for(int i=0; i<4; i++) {
-        int next_x = x + nx[i];
-        int next_y = y + ny[i];
-
-        if (0 <= next_x && next_x < h && 0 <= next_y && next_y < w){
-          if(visited[next_x][next_y] == false && maps[next_x][next_y] > 0) {
-            visited[next_x][next_y] = true;
-            maps[next_x][next_y] = maps[x][y] + 1;
-            q.add(new position(next_x, next_y));
-          }
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    
+    public int solution(int[][] maps) {
+        int answer = 0;
+        // 행의 길이, 열의 길이
+        int[][] visited = new int[maps.length][maps[0].length];
+        visited[0][0] = 1; // 시작 위치 방문체크
+        
+        // bfs 탐색
+        bfs(maps, visited);
+        
+        // 도착지 값을 넣어주기
+				// 배열의 인덱스는 0부터 시작하므로, 도착 위치(n-1, m-1)이 된다.
+        answer = visited[maps.length -1][maps[0].length -1];
+        
+        // 갈 수 없다면 -1 리턴
+        if (answer == 0) {
+            answer = -1;
         }
-      }
+        return answer;
     }
-    return maps[h-1][w-1] == 1 ? -1 : maps[h-1][w-1];
-  }
+    public void bfs(int[][] maps, int[][] visited) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0});
+        
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+            
+            // 4방 탐색
+            for(int i = 0; i < 4; i++) {
+                // 이동했을 때 위치
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                // 범위 벗어나는지 체크
+                // 방문했는지 체크
+                // 갈 수 있는 곳인지 체크
+                if(nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && visited[nx][ny] == 0 && maps[nx][ny] == 1) {
+                    // 방문했다고 체크해주기
+                    visited[nx][ny] = visited[x][y] + 1;
+                    // 큐에 넣기
+                    q.add(new int[]{nx, ny});
+                } 
+            }
+        }
+    }
 }
