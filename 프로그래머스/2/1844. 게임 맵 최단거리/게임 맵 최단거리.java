2566ad88fed0d932d09efe1a18cf5a1ab1d98e82
Junyong Moon <jymoon1121@gmail.com>
2023-12-16 17:45:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.79 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -5,48 +5,38 @@ class Solution {
     int[] dy = {0, 0, -1, 1};
     
     public int solution(int[][] maps) {
-        int answer = 0;
-        // 행의 길이, 열의 길이
-        int[][] visited = new int[maps.length][maps[0].length];
-        visited[0][0] = 1; // 시작 위치 방문체크
-        
-        // bfs 탐색
-        bfs(maps, visited);
-        
-        // 도착지 값을 넣어주기
-				// 배열의 인덱스는 0부터 시작하므로, 도착 위치(n-1, m-1)이 된다.
-        answer = visited[maps.length -1][maps[0].length -1];
-        
-        // 갈 수 없다면 -1 리턴
-        if (answer == 0) {
-            answer = -1;
-        }
-        return answer;
+       int answer = 0;
+       int[][] visited = new int[maps.length][maps[0].length];
+       visited[0][0] = 1; 
+
+       bfs(maps, visited);
+
+       answer = visited[maps.length - 1][maps[0].length -1];
+
+       if(answer == 0) {
+        answer = -1;
+       }
+
+       return answer;
     }
     public void bfs(int[][] maps, int[][] visited) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0});
-        
-        while(!q.isEmpty()) {
-            int[] now = q.poll();
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
             int x = now[0];
             int y = now[1];
-            
-            // 4방 탐색
+
             for(int i = 0; i < 4; i++) {
-                // 이동했을 때 위치
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                
-                // 범위 벗어나는지 체크
-                // 방문했는지 체크
-                // 갈 수 있는 곳인지 체크
-                if(nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    // 방문했다고 체크해주기
+
+                if(nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length
+                && visited[nx][ny] == 0 && maps[nx][ny] == 1) {
                     visited[nx][ny] = visited[x][y] + 1;
-                    // 큐에 넣기
-                    q.add(new int[]{nx, ny});
-                } 
+                    queue.add(new int[]{nx, ny});
+                }
             }
         }
     }
