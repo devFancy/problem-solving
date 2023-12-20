b457ccfc5a63c014010c5523f0120be205012480
Junyong Moon <jymoon1121@gmail.com>
2023-12-20 21:41:30 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,6 @@
+select board_id, writer_id, title, price, case when status = 'SALE' then '판매중' when status = 'RESERVED' 
+then '예약중' when status = 'DONE' 
+then '거래완료' end status
+from used_goods_board
+where board_id in(select board_id from used_goods_board where created_date = '2022-10-05')
+order by board_id desc;
